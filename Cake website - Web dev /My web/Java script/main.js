71b1b310cbbbde358b4f8938a5eba8811bdbb704
csmaianh <maianh521@icloud.com>
2025-08-22 14:00:27 +0700

    Add files via upload

---
+++ Cake website - Web dev /My web/Java script/main.js
@@ -0,0 +1,14 @@
+// Read more or Read less button 
+const readMoreButton = document.querySelector('.read-more-button');
+const text = document.querySelector('.text');
+
+readMoreButton.addEventListener('click',(e)=>{
+    text.classList.toggle('show-more');
+if (readMoreButton.innerText === 'Read More'){
+    readMoreButton.innerText = 'Read Less';
+
+}else {
+    readMoreButton.innerText = 'Read More';
+}
+
+})
